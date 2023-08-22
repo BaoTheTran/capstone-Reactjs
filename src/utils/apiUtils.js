b4c57a4fc0ba2ea449c3d8ff0962d23d47c1ea7d
BaoTheTran <ttbao2710@gmail.com>
2023-08-22 15:49:44 +0700

    complete project

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -7,16 +7,15 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
     const accessTokenUser = localStorage.getItem('USER_CUSTOMER') ?
         JSON.parse(localStorage.getItem('USER_CUSTOMER')).accessToken : ''
-    const accessTokenAdmin = localStorage.getItem('USER_ADMIN') ?
-        JSON.parse(localStorage.getItem('USER_ADMIN')).accessToken
-        : '';
+    // const accessTokenAdmin = localStorage.getItem('USER_ADMIN') ?
+    //     JSON.parse(localStorage.getItem('USER_ADMIN')).accessToken
+    //     : '';
 
 
     config.headers = {
         ...config.headers,
         TokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MCIsIkhldEhhblN0cmluZyI6IjE4LzAxLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwNTUzNjAwMDAwMCIsIm5iZiI6MTY3NzQzMDgwMCwiZXhwIjoxNzA1NjgzNjAwfQ.s4X0R0Wi80X0f9MLJ2XYxRKJdQJBW27dwvkpfN03100",
-        Authorization: `Bearer ${accessTokenUser}`,
-        Authorization: `Bearer ${accessTokenAdmin}`
+        Authorization: `Bearer ${accessTokenUser}`
     }
     return config
 });
