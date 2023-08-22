45d8d9d22c423ee9d1172900a6712846bfdf692e
BaoTheTran <ttbao2710@gmail.com>
2023-08-22 15:22:57 +0700

    complete project

--- src/page/HomeTemplate/giaVe/index.js
+++ src/page/HomeTemplate/giaVe/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
+
 export default class GiaVe extends Component {
   render() {
     return (
       <div className='container text-center'>
         <div className='content_giaVe'>
           <h1 className='title-gia-ve'>BẢNG GIÁ VÉ XEM PHIM 2D</h1>
-          <img src="http://ddcinema.vn/Areas/Admin/Content/Fileuploads/images/bang%20gia%20ve%202D%202023.jpg" className='img-fluid' />
+          <img src={require("../../../img/bang gia ve 2D 2023.jpg")} className='img-fluid' />
           <p>* Vé mời sẽ không được áp dụng vào các suất chiếu đặc biệt và các ngày Lễ, Tết theo qui định của Bảng giá.</p>
           <p>* Giá vé có thể thay đổi theo từng thời điểm. Mong Quý Khán giả thông cảm !</p>
         </div>       
